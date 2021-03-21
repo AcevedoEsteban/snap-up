d6ace8ac9b61b40a0a7689a08d45a8298b328d39
astevenaa <astevenaa@gmail.com>
2021-03-21 15:14:20 -0700

    added code editor packages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react'
+import Routes from './routes'
+import {Link} from 'react-router-dom'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div>
+     <nav>
+       <ul>
+         <li><Link to='/'> react flask </Link></li>
+       </ul>
+       <ul>
+         <li><Link to='/create'>new post</Link></li>
+       </ul>
+     </nav>
+     <main>
+       <Routes/>
+     </main>
     </div>
   );
 }
