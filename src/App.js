ccfb2f7480545330bab0acfcd0dc37c2ff882b72
astevenaa <astevenaa@gmail.com>
2021-03-23 20:38:28 -0700

    added css and routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React from 'react'
-import Routes from './routes'
-import {Link} from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Routes from './routes';
 
 import './App.css';
 
 function App() {
   return (
-   <div>
-     <nav>
-       <ul>
-         <li><Link to='/'> react flask </Link></li>
-       </ul>
-       <ul>
-         <li><Link to='/create'>new post</Link></li>
-       </ul>
-     </nav>
-     <main>
-       <Routes/>
-     </main>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/"> react flask </Link>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <Link to="/create">new post</Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <Routes />
+      </main>
     </div>
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -1,1 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Main from './components/main';
+import Create from './components/create';
+import Item from './components/item';
+
+const Routes = () => (
+  <Switch>
+    <Route exact path="/" component={Main} />
+    <Route exact path="/create" component={Create} />
+    <Route exact path="/item/:id" component={Item} />
+  </Switch>
+);
+
+export default Routes;
